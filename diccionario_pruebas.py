92ea9320028042d4f57ff5b186040a086b0bcba5
slazaroh02 <113343498+slazaroh02@users.noreply.github.com>
2022-09-12 12:12:18 +0200

    Create diccionario_pruebas.py

---
+++ diccionario_pruebas.py
@@ -0,0 +1,10 @@
+#creamos el diccionario
+lenguajes ={ 'juan':'python',
+             'carmen':'ruby',
+             'sara':'c',
+             'luis':'python',
+}
+#mostramos todas las claves del diccionario
+print("Las claves del diccionario son:")
+for clave, valor in lenguajes.items():
+    print(clave)
